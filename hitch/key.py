072aa3849970f9999f5d530702d3646a0313e2a4
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-06-16 18:48:13 +0100

    REFACTOR : Ran black.

--- hitch/key.py
+++ hitch/key.py
@@ -2,13 +2,17 @@ from hitchstory import StoryCollection, StorySchema, BaseEngine, HitchStoryExcep
 from hitchstory import validate, expected_exception
 from hitchrun import expected
 from icommandlib import ICommand, ICommandError
-from commandlib import Command, CommandError, python
+from commandlib import Command, CommandError, python, python_bin
 from strictyaml import Str, Map, MapPattern, Bool, Optional, load
 from pathquery import pathquery
 from hitchrun import hitch_maintenance
 from hitchrun import DIR
 from hitchrun.decorators import ignore_ctrlc
-from hitchrunpy import ExamplePythonCode, HitchRunPyException, ExpectedExceptionMessageWasDifferent
+from hitchrunpy import (
+    ExamplePythonCode,
+    HitchRunPyException,
+    ExpectedExceptionMessageWasDifferent,
+)
 import requests
 from templex import Templex, NonMatching
 from path import Path
@@ -18,20 +22,21 @@ import signal
 
 
 def project_build(paths, python_version, selenium_version=None):
-    pylibrary = hitchbuildpy.PyLibrary(
-        name="py{0}".format(python_version),
-        base_python=hitchbuildpy.PyenvBuild(python_version)
-                                .with_build_path(paths.share),
-        module_name="seleniumdirector",
-        library_src=paths.project,
-    ).with_requirementstxt(
-        paths.key/"debugrequirements.txt"
-    ).with_build_path(paths.gen)
+    pylibrary = (
+        hitchbuildpy.PyLibrary(
+            name="py{0}".format(python_version),
+            base_python=hitchbuildpy.PyenvBuild(python_version).with_build_path(
+                paths.share
+            ),
+            module_name="seleniumdirector",
+            library_src=paths.project,
+        )
+        .with_requirementstxt(paths.key / "debugrequirements.txt")
+        .with_build_path(paths.gen)
+    )
 
     if selenium_version is not None:
-        pylibrary = pylibrary.with_packages(
-            "selenium=={0}".format(selenium_version)
-        )
+        pylibrary = pylibrary.with_packages("selenium=={0}".format(selenium_version))
 
     pylibrary.ensure_built()
     return pylibrary
@@ -50,9 +55,7 @@ class Engine(BaseEngine):
             Optional("setup"): Str(),
             Optional("code"): Str(),
         },
-        info={
-            Optional("docs"): Str(),
-        },
+        info={Optional("docs"): Str()},
     )
 
     def __init__(self, keypath, settings):
@@ -68,54 +71,57 @@ class Engine(BaseEngine):
 
         self.path.profile = self.path.gen.joinpath("profile")
         dirtemplate.DirTemplate(
-            "webapp",
-            self.path.key/"htmltemplate",
-            self.path.state,
-        ).with_vars(
-            javascript=self.given.get("javascript", "")
-        ).with_files(
+            "webapp", self.path.key / "htmltemplate", self.path.state
+        ).with_vars(javascript=self.given.get("javascript", "")).with_files(
             base_html={
-                filename: {"content": content} for filename, content in
-                self.given.get("website", {}).items()
-            },
+                filename: {"content": content}
+                for filename, content in self.given.get("website", {}).items()
+            }
         ).ensure_built()
 
-        self.path.state.joinpath("selectors.yml").write_text(self.given['selectors.yml'])
+        self.path.state.joinpath("selectors.yml").write_text(
+            self.given["selectors.yml"]
+        )
 
-        self.server = python("-m", "http.server").in_dir(self.path.state/"webapp").pexpect()
+        self.server = (
+            python("-m", "http.server").in_dir(self.path.state / "webapp").pexpect()
+        )
         self.server.expect("Serving HTTP on 0.0.0.0")
 
         if not self.path.profile.exists():
             self.path.profile.mkdir()
 
         self.python = project_build(
-            self.path,
-            self.given['python version'],
-            self.given['selenium version'],
+            self.path, self.given["python version"], self.given["selenium version"]
         ).bin.python
 
-        self.example_py_code = ExamplePythonCode(self.python, self.path.state)\
-            .with_code(self.given.get('code', ''))\
-            .with_setup_code(self.given.get('setup', ''))\
-            .with_terminal_size(160, 100)\
+        self.example_py_code = (
+            ExamplePythonCode(self.python, self.path.state)
+            .with_code(self.given.get("code", ""))
+            .with_setup_code(self.given.get("setup", ""))
+            .with_terminal_size(160, 100)
             .with_long_strings()
+        )
 
     @expected_exception(NonMatching)
     @expected_exception(HitchRunPyException)
     @validate(
         code=Str(),
         will_output=Map({"in python 2": Str(), "in python 3": Str()}) | Str(),
-        raises=Map({
-            Optional("type"): Map({"in python 2": Str(), "in python 3": Str()}) | Str(),
-            Optional("message"): Map({"in python 2": Str(), "in python 3": Str()}) | Str(),
-        }),
+        raises=Map(
+            {
+                Optional("type"): Map({"in python 2": Str(), "in python 3": Str()})
+                | Str(),
+                Optional("message"): Map({"in python 2": Str(), "in python 3": Str()})
+                | Str(),
+            }
+        ),
         in_interpreter=Bool(),
     )
     def run(self, code, will_output=None, raises=None, in_interpreter=False):
         if in_interpreter:
-            code = '{0}\nprint(repr({1}))'.format(
-                '\n'.join(code.strip().split('\n')[:-1]),
-                code.strip().split('\n')[-1]
+            code = "{0}\nprint(repr({1}))".format(
+                "\n".join(code.strip().split("\n")[:-1]), code.strip().split("\n")[-1]
             )
         to_run = self.example_py_code.with_code(code)
 
@@ -124,10 +130,14 @@ class Engine(BaseEngine):
                 self.path.profile.joinpath("{0}.dat".format(self.story.slug))
             )
 
-        result = to_run.expect_exceptions().run() if raises is not None else to_run.run()
+        result = (
+            to_run.expect_exceptions().run() if raises is not None else to_run.run()
+        )
 
         if will_output is not None:
-            actual_output = '\n'.join([line.rstrip() for line in result.output.split("\n")])
+            actual_output = "\n".join(
+                [line.rstrip() for line in result.output.split("\n")]
+            )
             try:
                 Templex(will_output).assert_match(actual_output)
             except NonMatching:
@@ -138,22 +148,26 @@ class Engine(BaseEngine):
 
         if raises is not None:
             differential = False  # Difference between python 2 and python 3 output?
-            exception_type = raises.get('type')
-            message = raises.get('message')
+            exception_type = raises.get("type")
+            message = raises.get("message")
 
             if exception_type is not None:
                 if not isinstance(exception_type, str):
                     differential = True
-                    exception_type = exception_type['in python 2']\
-                        if self.given['python version'].startswith("2")\
-                        else exception_type['in python 3']
+                    exception_type = (
+                        exception_type["in python 2"]
+                        if self.given["python version"].startswith("2")
+                        else exception_type["in python 3"]
+                    )
 
             if message is not None:
                 if not isinstance(message, str):
                     differential = True
-                    message = message['in python 2']\
-                        if self.given['python version'].startswith("2")\
-                        else message['in python 3']
+                    message = (
+                        message["in python 2"]
+                        if self.given["python version"].startswith("2")
+                        else message["in python 3"]
+                    )
 
             try:
                 result = self.example_py_code.expect_exceptions().run()
@@ -161,7 +175,7 @@ class Engine(BaseEngine):
             except ExpectedExceptionMessageWasDifferent as error:
                 if self.settings.get("rewrite") and not differential:
                     new_raises = raises.copy()
-                    new_raises['message'] = result.exception.message
+                    new_raises["message"] = result.exception.message
                     self.current_step.update(raises=new_raises)
                 else:
                     raise
@@ -171,6 +185,7 @@ class Engine(BaseEngine):
 
     def pause(self, message="Pause"):
         import IPython
+
         IPython.embed()
 
     def tear_down(self):
@@ -178,37 +193,35 @@ class Engine(BaseEngine):
         self.server.wait()
 
 
-
 def _storybook(settings):
     return StoryCollection(pathquery(DIR.key).ext("story"), Engine(DIR, settings))
 
 
 def _current_version():
-    return DIR.project.joinpath("VERSION").bytes().decode('utf8').rstrip()
+    return DIR.project.joinpath("VERSION").bytes().decode("utf8").rstrip()
 
 
 def _personal_settings():
     settings_file = DIR.key.joinpath("personalsettings.yml")
 
     if not settings_file.exists():
-        settings_file.write_text((
-            "engine:\n"
-            "  rewrite: no\n"
-            "  cprofile: no\n"
-            "params:\n"
-            "  python version: 3.5.0\n"
-        ))
+        settings_file.write_text(
+            (
+                "engine:\n"
+                "  rewrite: no\n"
+                "  cprofile: no\n"
+                "params:\n"
+                "  python version: 3.5.0\n"
+            )
+        )
     return load(
-        settings_file.bytes().decode('utf8'),
-        Map({
-            "engine": Map({
-                "rewrite": Bool(),
-                "cprofile": Bool(),
-            }),
-            "params": Map({
-                "python version": Str(),
-            }),
-        })
+        settings_file.bytes().decode("utf8"),
+        Map(
+            {
+                "engine": Map({"rewrite": Bool(), "cprofile": Bool()}),
+                "params": Map({"python version": Str()}),
+            }
+        ),
     )
 
 
@@ -218,10 +231,9 @@ def bdd(*keywords):
     Run stories matching keywords.
     """
     settings = _personal_settings().data
-    _storybook(settings['engine'])\
-        .with_params(**{"python version": settings['params']['python version']})\
-        .only_uninherited()\
-        .shortcut(*keywords).play()
+    _storybook(settings["engine"]).with_params(
+        **{"python version": settings["params"]["python version"]}
+    ).only_uninherited().shortcut(*keywords).play()
 
 
 @expected(HitchStoryException)
@@ -230,11 +242,10 @@ def rbdd(*keywords):
     Run stories matching keywords.
     """
     settings = _personal_settings().data
-    settings['rewrite'] = True
-    _storybook(settings['engine'])\
-        .with_params(**{"python version": settings['params']['python version']})\
-        .only_uninherited()\
-        .shortcut(*keywords).play()
+    settings["rewrite"] = True
+    _storybook(settings["engine"]).with_params(
+        **{"python version": settings["params"]["python version"]}
+    ).only_uninherited().shortcut(*keywords).play()
 
 
 @expected(HitchStoryException)
@@ -244,13 +255,13 @@ def regressfile(filename):
 
     Rewrite stories if appropriate.
     """
-    _storybook({"rewrite": False}).in_filename(filename)\
-                                  .with_params(**{"python version": "2.7.10"})\
-                                  .filter(lambda story: not story.info['fails on python 2'])\
-                                  .ordered_by_name().play()
+    _storybook({"rewrite": False}).in_filename(filename).with_params(
+        **{"python version": "2.7.10"}
+    ).filter(lambda story: not story.info["fails on python 2"]).ordered_by_name().play()
 
-    _storybook({"rewrite": False}).with_params(**{"python version": "3.5.0"})\
-                                  .in_filename(filename).ordered_by_name().play()
+    _storybook({"rewrite": False}).with_params(
+        **{"python version": "3.5.0"}
+    ).in_filename(filename).ordered_by_name().play()
 
 
 @expected(HitchStoryException)
@@ -296,14 +307,13 @@ def deploy(version):
     NAME = "seleniumdirector"
     git = Command("git").in_dir(DIR.project)
     version_file = DIR.project.joinpath("VERSION")
-    old_version = version_file.bytes().decode('utf8')
+    old_version = version_file.bytes().decode("utf8")
     if version_file.bytes().decode("utf8") != version:
         DIR.project.joinpath("VERSION").write_text(version)
         git("add", "VERSION").run()
-        git("commit", "-m", "RELEASE: Version {0} -> {1}".format(
-            old_version,
-            version
-        )).run()
+        git(
+            "commit", "-m", "RELEASE: Version {0} -> {1}".format(old_version, version)
+        ).run()
         git("push").run()
         git("tag", "-a", version, "-m", "Version {0}".format(version)).run()
         git("push", "origin", version).run()
@@ -312,17 +322,15 @@ def deploy(version):
 
     # Set __version__ variable in __init__.py, build sdist and put it back
     initpy = DIR.project.joinpath(NAME, "__init__.py")
-    original_initpy_contents = initpy.bytes().decode('utf8')
-    initpy.write_text(
-        original_initpy_contents.replace("DEVELOPMENT_VERSION", version)
-    )
+    original_initpy_contents = initpy.bytes().decode("utf8")
+    initpy.write_text(original_initpy_contents.replace("DEVELOPMENT_VERSION", version))
     python("setup.py", "sdist").in_dir(DIR.project).run()
     initpy.write_text(original_initpy_contents)
 
     # Upload to pypi
-    python(
-        "-m", "twine", "upload", "dist/{0}-{1}.tar.gz".format(NAME, version)
-    ).in_dir(DIR.project).run()
+    python("-m", "twine", "upload", "dist/{0}-{1}.tar.gz".format(NAME, version)).in_dir(
+        DIR.project
+    ).run()
 
 
 @expected(dirtemplate.exceptions.DirTemplateException)
@@ -330,32 +338,36 @@ def docgen():
     """
     Build documentation.
     """
+
     def title(dirfile):
-        assert len(dirfile.text().split("---")) >= 3, "{} doesn't have ---".format(dirfile)
+        assert len(dirfile.text().split("---")) >= 3, "{} doesn't have ---".format(
+            dirfile
+        )
         return load(dirfile.text().split("---")[1]).data.get("title", "misc")
 
-    docfolder = DIR.gen/"docs"
+    docfolder = DIR.gen / "docs"
 
     if docfolder.exists():
         docfolder.rmtree(ignore_errors=True)
     docfolder.mkdir()
 
-    template = dirtemplate.DirTemplate(
-        "docs", DIR.project/"docs", DIR.gen,
-    ).with_files(
-        story_md={
-            "using/alpha/{0}.md".format(story.info['docs']): {"story": story}
-            for story in _storybook({}).ordered_by_name()
-            if story.info.get("docs") is not None
-        },
-    ).with_vars(
-        readme=False,
-        quickstart=_storybook({})
-        .in_filename(DIR.key/"quickstart.story")
-        .non_variations()
-        .ordered_by_file(),
-    ).with_functions(
-        title=title
+    template = (
+        dirtemplate.DirTemplate("docs", DIR.project / "docs", DIR.gen)
+        .with_files(
+            story_md={
+                "using/alpha/{0}.md".format(story.info["docs"]): {"story": story}
+                for story in _storybook({}).ordered_by_name()
+                if story.info.get("docs") is not None
+            }
+        )
+        .with_vars(
+            readme=False,
+            quickstart=_storybook({})
+            .in_filename(DIR.key / "quickstart.story")
+            .non_variations()
+            .ordered_by_file(),
+        )
+        .with_functions(title=title)
     )
     template.ensure_built()
     print("Docs generated")
@@ -389,6 +401,14 @@ def hvenvup(package, directory):
     pip = Command(DIR.gen.joinpath("hvenv", "bin", "pip"))
     pip("uninstall", package, "-y").run()
     pip("install", DIR.project.joinpath(directory).abspath()).run()
+
+
+def black():
+    """
+    Reformat the code.
+    """
+    python_bin.black(DIR.project / "seleniumdirector").run()
+    python_bin.black(DIR.key / "key.py").run()
 
 
 def rerun(version="3.5.0"):
--- seleniumdirector/webselector.py
+++ seleniumdirector/webselector.py
@@ -30,6 +30,7 @@ class text_to_be_present_in_element_contents_or_value(Expectation):
     An expectation for checking if the given text is present in the element's
     text or its value attribute.
     """
+
     def check(self, element):
         if self.text in element.text:
             return True
@@ -67,22 +68,16 @@ class WebElement(object):
 
     def should_contain(self, text):
         WebDriverWait(self._director.driver, self._director.default_timeout).until(
-            text_to_be_present_in_element_contents_or_value(
-                self._selector, text
-            )
+            text_to_be_present_in_element_contents_or_value(self._selector, text)
         )
 
     def should_appear(self):
         WebDriverWait(self._director.driver, self._director.default_timeout).until(
-            expected_conditions.visibility_of_element_located(
-                self._selector
-            )
+            expected_conditions.visibility_of_element_located(self._selector)
         )
         if len(self._director.driver.find_elements_by_xpath(self.identifier)) > 1:
             raise exceptions.MoreThanOneElement(
-                "More than one element matches your query '{}'.".format(
-                    self.identifier,
-                )
+                "More than one element matches your query '{}'.".format(self.identifier)
             )
 
 
@@ -93,18 +88,20 @@ class WebSelector(object):
             Path(selector_file).text(),
             MapPattern(
                 Str(),
-                Map({
-                    "appears when": Str(),
-                    "elements": MapPattern(
-                        Str(),
-                        Str() | \
-                        Map({"attribute": Str()}) | \
-                        Map({"xpath": Str()}) | \
-                        Map({"text contains": Str()}) | \
-                        Map({"text is": Str()}),
-                    ),
-                }),
-            )
+                Map(
+                    {
+                        "appears when": Str(),
+                        "elements": MapPattern(
+                            Str(),
+                            Str()
+                            | Map({"attribute": Str()})
+                            | Map({"xpath": Str()})
+                            | Map({"text contains": Str()})
+                            | Map({"text is": Str()}),
+                        ),
+                    }
+                ),
+            ),
         ).data
         self._current_page = None
         self.default_timeout = 5
@@ -114,57 +111,57 @@ class WebSelector(object):
         if fake_time is not None:
             assert isinstance(self.driver, Chrome)
             response = self.driver.command_executor._request(
-                'POST',
+                "POST",
                 "{}/session/{}/chromium/send_command_and_get_result".format(
-                    driver.command_executor._url,
-                    driver.session_id,
+                    driver.command_executor._url, driver.session_id
                 ),
-                json.dumps({
-                    'cmd': "Page.addScriptToEvaluateOnNewDocument",
-                    'params': {
-                        "source": "{}\n{}".format(
-                            THIS_DIRECTORY.joinpath("timemachine.js").text(),
-                            "timemachine.config({timestamp: " + str(
-                                fake_time.timestamp() * 1000
-                            ) + "});"
-                        )
+                json.dumps(
+                    {
+                        "cmd": "Page.addScriptToEvaluateOnNewDocument",
+                        "params": {
+                            "source": "{}\n{}".format(
+                                THIS_DIRECTORY.joinpath("timemachine.js").text(),
+                                "timemachine.config({timestamp: "
+                                + str(fake_time.timestamp() * 1000)
+                                + "});",
+                            )
+                        },
                     }
-                })
+                ),
             )
-            assert response['status'] == 0, \
-                "Error faking time {}".format(str(response))
+            assert response["status"] == 0, "Error faking time {}".format(str(response))
 
     def visit(self, url):
         self.driver.get(url)
 
     def wait_for_page(self, page_name):
-        appears_when = self._selectors[page_name]['appears when']
+        appears_when = self._selectors[page_name]["appears when"]
         self._select(appears_when, page_name).should_appear()
         self._current_page = page_name
 
     def _select(self, name, page):
-        element_yaml = self._selectors[page]['elements'][name]
+        element_yaml = self._selectors[page]["elements"][name]
         if isinstance(element_yaml, dict):
             if "attribute" in element_yaml.keys():
                 key, value = element_yaml["attribute"].split("=")
-                return WebElement(self, "xpath", "(//*[@{0}='{1}'])[1]".format(key, value))
+                return WebElement(
+                    self, "xpath", "(//*[@{0}='{1}'])[1]".format(key, value)
+                )
             if "xpath" in element_yaml.keys():
-                return WebElement(self, "xpath", element_yaml['xpath'])
+                return WebElement(self, "xpath", element_yaml["xpath"])
             if "text is" in element_yaml.keys():
                 return WebElement(
                     self,
                     "xpath",
-                    '(//*[text()="{}"])[1]'.format(
-                        element_yaml['text is']
-                    )
+                    '(//*[text()="{}"])[1]'.format(element_yaml["text is"]),
                 )
             if "text contains" in element_yaml.keys():
                 return WebElement(
                     self,
                     "xpath",
                     '(//*[contains(text(), "{}")])[1]'.format(
-                        element_yaml['text contains']
-                    )
+                        element_yaml["text contains"]
+                    ),
                 )
         else:
             seltype, ident = element_yaml.split("=")
